3329d906de0b89570a23edc8f114c8155d6531c5
Cristian David Gutierrez <cdgutierrez456@gmail.com>
2022-06-29 21:23:59 -0500

    Haciendo uso de acceso por medio de login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  constructor( private fb: FormBuilder,
+               private authService: AuthService,
+               private router: Router ) { }
 
   ngOnInit(): void {
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+
+  }
+
+  validateUser() {
+    if (this.loginForm.invalid) {return;}
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.loginUser(email, password)
+      .then( credentials => {
+        console.log('Credentials ', credentials)
+        this.router.navigate(['/'])
+      })
+      .catch(error => console.log(error))
   }
 
 }
